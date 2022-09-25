c305b0fa098b3ed1760b21db7e4d532d4b204f94
Joseph Ganya <josephganya930@gmail.com>
2022-09-25 16:04:38 +0100

    setup render and delte functions with dummy array

---
+++ app.js
@@ -0,0 +1,85 @@
+const display_books=document.querySelector('#book-display');
+
+
+
+//static library for test
+let myLibrary=[
+    {
+    author:'jo',
+    title:'how to make it',
+    pagesRead:20,
+    read:'yes'
+},
+{
+    author:'pau',
+    title:'how to make it',
+    pagesRead:20,
+    read:'no'
+},
+{
+    author:'ganya',
+    title:'how to make it',
+    pagesRead:10,
+    read:'no'
+}
+
+];
+
+//function to create book element and append to div 
+function createBookElement(book,index){
+   //create div
+    const div=document.createElement('div');
+    div.setAttribute('id',index);
+    div.classList.add('.books');
+    //create book author element and append to div
+    const book_author=document.createElement('div');
+    book_author.setAttribute('id',`author${index}`);
+    book_author.textContent=`Author:${myLibrary[index].author}`;
+    div.appendChild(book_author);
+    //create book title element and appned to div
+    const book_title=document.createElement('div');
+    book_title.setAttribute('id',`title${index}`);
+    book_title.textContent=`Author:${myLibrary[index].title}`;
+    div.appendChild(book_title);
+    //Create  book page element an append to div
+    const book_pages=document.createElement('div');
+    book_pages.setAttribute('id',`pages${index}`);
+    book_pages.textContent=`Author:${myLibrary[index].pagesRead}`;
+    div.appendChild(book_pages);
+    //create checkbox element and append to div
+    const book_checkbox=document.createElement('input');
+    book_checkbox.setAttribute('id',`checkbox${index}`);
+    book_checkbox.setAttribute('type','checkbox');
+    div.appendChild(book_checkbox);
+    //create delete button element
+    const delete_button=document.createElement('div');
+    delete_button.textContent='Delete';
+    delete_button.setAttribute('id',`delete_button${index}`);
+    delete_button.addEventListener('click',deleteBook)
+    div.appendChild(delete_button);
+
+//append div to html element
+    display_books.appendChild(div);
+
+
+};
+
+//delete book function
+function deleteBook(index){
+    myLibrary.splice(index,1);
+    console.log(myLibrary)
+    renderBooks() 
+}
+//function to render books from array library to the display
+function renderBooks(){
+
+    display_books.innerHTML='';
+   myLibrary.map((book,index)=>{
+ createBookElement(book,index)
+
+
+
+    });
+};
+
+renderBooks()
