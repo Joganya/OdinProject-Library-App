3f3259847602610ef03c528b8077234afb629c03
Joseph Ganya <josephganya930@gmail.com>
2022-09-28 15:03:41 +0100

    fixed checkbox status

--- app.js
+++ app.js
@@ -1,45 +1,27 @@
 const display_books=document.querySelector('#book-display');
 const display_modal=document.querySelector('#add-button');
 const modal=document.querySelector('#modal-background');
+const getAuthor=document.querySelector('#author');
+const getTitle=document.querySelector('#title');
+const getPages=document.querySelector('#pages');
+const getStatus=document.querySelector('#status');
+const submitModal=document.querySelector('#submit-modal');
+
 
 
 
 
 //static library for test
-let myLibrary=[
-    {
-    author:'jo',
-    title:'how to make it',
-    pagesRead:20,
-    read:'yes'
-},
-{
-    author:'pau',
-    title:'how to make it',
-    pagesRead:20,
-    read:'no'
-},
-{
-    author:'ganya',
-    title:'how to make it',
-    pagesRead:10,
-    read:'no'
-},
-{
-    author:'ganya',
-    title:'how to make it',
-    pagesRead:10,
-    read:'no'
-},
-{
-    author:'ganya',
-    title:'how to make it',
-    pagesRead:10,
-    read:'no'
-}
+let myLibrary=[];
+//creator function
+function Books(author,title,numberOfPages,read){
+    this.author=author,
+    this.title=title,
+    this.numberOfPages=numberOfPages,
+    this.read=read
+};
 
 
-];
 
 //function to create book element and append to div 
 function createBookElement(book,index){
@@ -74,7 +56,7 @@ function createBookElement(book,index){
     const book_pages=document.createElement('div');
     book_pages.setAttribute('id',`pages${index}`);
     book_pages.setAttribute('class','card-text');
-    book_pages.textContent=`PagesRead:${myLibrary[index].pagesRead}`;
+    book_pages.textContent=`Number Of Pages:${myLibrary[index].numberOfPages}`;
     div.appendChild(book_pages);
     //create checkbox div
     const theCheckBox=document.createElement('div');
@@ -86,7 +68,9 @@ function createBookElement(book,index){
     const book_checkbox=document.createElement('input');
     book_checkbox.setAttribute('id',`checkbox${index}`);
     book_checkbox.setAttribute('type','checkbox');
+    book_checkbox.checked=myLibrary[index].read;
     theCheckBox.appendChild(book_checkbox);
+    
     //create delete button element
     const delete_button=document.createElement('div');
     delete_button.textContent='Delete';
@@ -122,5 +106,16 @@ function renderBooks(){
 
     });
 };
+//add to array and display book item
+function pushBook(){
+    myLibrary.push(new Books((getAuthor.value),(getTitle.value),(getPages.value),(getStatus.checked)))
+    renderBooks();
+    modal.style.display='none'
+};
 
-renderBooks()
+//call book function on click
+submitModal.addEventListener('click',()=>{
+    pushBook();
+    console.log(getPages.value);
+})
+
